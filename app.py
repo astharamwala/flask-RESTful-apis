bb04b8a0d5c54312cad53f0947f417be62b09596
code2-tech <astha.ramwala@gmail.com>
2020-01-20 11:49:43 +0530

    First commit, created REST apis of model, resources with authentication

---
+++ app.py
@@ -0,0 +1,40 @@
+import logging
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
+
+from security import authentication, identify
+from resources.item import Item, ItemList
+from resources.user import UserRegister
+from resources.store import Store, StoreList
+
+logger = logging.getLogger("ENV OF LOGGER")
+
+logger.debug("Flask app starts now")
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDatabase.db'
+
+api = Api(app)
+app.secret_key = "SomeSecretKey"
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+jwt = JWT(app, authentication, identify)
+
+api.add_resource(Store,"/store/<name>")
+api.add_resource(Item, "/item/<name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(StoreList, "/stores")
+api.add_resource(UserRegister, '/register')
+
+if __name__ == "__main__":
+    from db import db
+
+    db.init_app(app)
+    app.run(debug=True)
---
+++ security.py
@@ -0,0 +1,12 @@
+from resources.user import UserModel
+
+
+def authentication(username, password):
+    user = UserModel.find_by_username(username)
+    if user and user.password == password:
+        return user
+
+
+def identify(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_user_id(user_id)
